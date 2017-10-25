1996ae91e0d0c0e291460878eeb18d1b785e656f
fery ferlangga <pengusahaads@gmail.com>
2017-10-25 18:42:00 +0700

    Update ffery.js

--- ffery.js
+++ ffery.js
@@ -22,8 +22,8 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="amp-safelink.html"}
-if(x=="2"){xxx="convert-amp-blogger.html"}
-if(x=="3"){xxx="safelink-amp-blogger.html"}
+if(x=="2"){xxx="safelink-amp-blogger.html"}
+if(x=="3"){xxx="convert-amp-blogger.html"}
 if(x=="4"){xxx="belajar-amp-safelink.html"}a_to_ve[i].href="https://ampsafelink.blogspot.com/2017/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
