ca1e03869b9936ec7601d17fcb74792e96653ea7
fery ferlangga <pengusahaads@gmail.com>
2020-04-21 18:15:38 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="amp-safelink.html"}
 if(x=="2"){xxx="safelink-amp-blogger.html"}
 if(x=="3"){xxx="convert-amp-blogger.html"}
-if(x=="4"){xxx="belajar-amp-safelink.html"}a_to_ve[i].href="https://www.fflink.ga/2017/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="belajar-amp-safelink.html"}a_to_ve[i].href="https://ampsafelink.blogspot.com/2017/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
